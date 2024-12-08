f73c04a3a1fdd438566e44927a299ba137185484
Eric Caro <c.aroericm@gmail.com>
2024-12-08 12:30:47 -0600

    comment

---
+++ RerunTest/Program.cs
@@ -0,0 +1,33 @@
+//ASP.NET standers for Active Server Pages.NET. ASP is a web framework developed by Microsoft for building dynamic web pages and services. It allows users to build and deploy robust, scalable, and intereactive web pages using languages such as c# and vb.NET
+fhfhfhf fugfgfgff 
+
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
